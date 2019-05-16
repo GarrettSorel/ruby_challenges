9189f0fb95111ce88adda19e9b9814147563f38a
GarrettSorel <garrettsorel@gmail.com>
2019-05-16 13:04:00 -0400

    completed caesar cipher

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,3 +1,14 @@
 def caesar_cipher(str, num)
-  new_str = str
+  alphabet = Array('a'..'z')
+  non_caps = Hash[alphabet.zip(alphabet.rotate(num))]
+
+  alphabet = Array('A'..'Z')
+  caps = Hash[alphabet.zip(alphabet.rotate(num))]
+
+  encrypter = non_caps.merge(caps)
+
+  str.chars.map { |c| encrypter.fetch(c, c) }
 end
+
+puts caesar_cipher("Garrett", 7).join
+puts caesar_cipher("The Cow jumped over the Moon", 4).join

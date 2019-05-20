367518842e3c04b9545377d35f4489267b13dd0d
GarrettSorel <garrettsorel@gmail.com>
2019-05-20 13:04:48 -0400

    completed stock picker

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,14 +1,22 @@
 def caesar_cipher(str, num)
+  #create a Hash with lowercase letters corresponding the the number provided
+  #example: 5, {a => e}
   alphabet = Array('a'..'z')
   non_caps = Hash[alphabet.zip(alphabet.rotate(num))]
 
+  #create a Hash with uppercase letters corresponding the the number provided
+  #example: 5, {A => E}
   alphabet = Array('A'..'Z')
   caps = Hash[alphabet.zip(alphabet.rotate(num))]
 
+
+  #merge two Hashes together
   encrypter = non_caps.merge(caps)
 
+  #turn the string into an array, and transform array elements from Hash
   str.chars.map { |c| encrypter.fetch(c, c) }
 end
+
 
 puts caesar_cipher("Garrett", 7).join
 puts caesar_cipher("The Cow jumped over the Moon", 4).join
---
+++ stock_picker/stock_picker.rb
@@ -0,0 +1,27 @@
+#pass an array of numbers
+#iterate over the array and compare which numbers have the highest value
+#store the value and the indexes of numbers
+#return the answer array
+
+def stock_picker(arr)
+  answer = []
+  highest_value = 0
+  buy = 0
+  sell = 0
+
+  for i in 0..(arr.length-2) do
+    for j in (i+1)..(arr.length-1) do
+      if arr[j] - arr[i] > highest_value
+        highest_value = arr[j] - arr[i]
+        buy = i
+        sell = j
+      end
+    end
+  end
+  answer.push(buy)
+  answer.push(sell)
+
+  puts answer
+end
+
+stock_picker([3,6,10,1,7,6,17,13,5])
